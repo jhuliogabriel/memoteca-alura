948d3e80cc97be47278926dbccf2eee21da49f24
jhuliogabriel <51342605+jhuliogabriel@users.noreply.github.com>
2023-07-03 15:59:58 -0300

    Add files via upload
    commit inicial

---
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-listar-pensamentos',
+  templateUrl: './listar-pensamentos.component.html',
+  styleUrls: ['./listar-pensamentos.component.css']
+})
+export class ListarPensamentosComponent implements OnInit{
+
+  listaPensamentos: Pensamento[] = []; //A lista virá do backend
+  paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
+  filtro: string = ''
+  favoritos: boolean = false
+  listaFavoritos: Pensamento[] = []
+  titulo: string = 'Meu Mural'
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+    ) { }
+
+  ngOnInit(): void {
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos).subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    }) 
+  }
+
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual, this.filtro, this.favoritos)
+    .subscribe(listaPensamentos =>{
+      this.listaPensamentos.push(...listaPensamentos);
+
+      if(this.listaPensamentos.length){
+        this.haMaisPensamentos = false
+      }
+    })
+  }
+
+  pesquisarPensamentos(){
+    this.haMaisPensamentos = true;
+    this.paginaAtual = 1;
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos)
+      .subscribe(listaPensamentos => {
+        this.listaPensamentos = listaPensamentos
+      })
+  }
+
+  listarFavoritos(){
+    this.titulo = 'Meus favoritos'
+    this.favoritos = true
+    this.haMaisPensamentos = true
+    this.paginaAtual = 1
+    this.service.listar(this.paginaAtual, this.filtro, this.favoritos)
+      .subscribe(listaPensamentosFavoritos => {
+        this.listaPensamentos = listaPensamentosFavoritos
+        this.listaFavoritos  = listaPensamentosFavoritos
+      })
+  }
+
+  recarregarComponente(){
+    this.favoritos = false;
+    this.paginaAtual = 1;
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload'
+    this.router.navigate([this.router.url])
+  }
+
+}
